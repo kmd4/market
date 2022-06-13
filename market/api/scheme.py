6086f7f974719e2f7de63f4668fe99d475104f16
kmd4 <mdku2005@gmail.com>
2022-06-13 23:40:00 +0300

    4

--- market/api/scheme.py
+++ market/api/scheme.py
@@ -4,69 +4,114 @@ from marshmallow import Schema, ValidationError, validates, validates_schema
 from marshmallow.fields import Date, Dict, Float, Int, List, Nested, Str
 from marshmallow.validate import Length, OneOf, Range
 
+from market.api.shoptype import ShopUnitType
 
-BIRTH_DATE_FORMAT = '%d.%m.%Y'
+
+DATE_FORMAT = '%D.%M.%YT%h:%M:%S.%Z'
 
 
-#Базовый класс для товаров/категорий, проверка на корректность введенной даты
-class ShopThings(Schema):
-    id=Str(nullable=False)
-    name = Str(nullable=True)
-    date = Date(format=BIRTH_DATE_FORMAT)
-    parentId = Str(nullable=True)
-    type=Str(validate=OneOf([unit.value for unit in ShopUnitType]))
-    price=Int(validate=Range(min=0), strict=True, nullable=True)
-    children=List(Dict(nullable=False))
-
-    @validates('date')
-    def validate_date(self, value: date):
-        if value > date.today():
-            raise ValidationError("Upgrade date can't be in future")
-
-    @validates('relatives')
-    def validate_relatives_unique(self, value: list):
-        if len(value) != len(set(value)):
-            raise ValidationError('relatives must be unique')
-
-# class ShopUnit(Schema):
-#     id = Str($uuid)
-#     nullable: false
-#     example: 3
-#     fa85f64 - 5717 - 4562 - b3fc - 2
-#     c963f66a333
-#     Уникальный
-#     идентфикатор
+# class PatchCitizenSchema(Schema):
+#     name = Str(validate=Length(min=1, max=256))
+#     type = Str(validate=OneOf([types.value for types in ShopUnitType]))
+#     birth_date = Date(format=BIRTH_DATE_FORMAT)
+#     town = Str(validate=Length(min=1, max=256))
+#     street = Str(validate=Length(min=1, max=256))
+#     building = Str(validate=Length(min=1, max=256))
+#     price = Int(validate=Range(min=0), strict=True)
+#     relatives = List(Int(validate=Range(min=0), strict=True))
 #
-#     name * string
-#     nullable: false
-#     Имя
-#     элемента.
+#     @validates('birth_date')
+#     def validate_birth_date(self, value: date):
+#         if value > date.today():
+#             raise ValidationError("Birth date can't be in future")
 #
-#     parentId
-#     string($uuid)
-#     nullable: true
-#     example: 3
-#     fa85f64 - 5717 - 4562 - b3fc - 2
-#     c963f66a333
-#     UUID
-#     родительской
-#     категории
-#
-#     type * ShopUnitTypestring
-#     Тип
-#     элемента - категория
-#     или
-#     товар
-#
-#     Enum:
-#     Array[2]
-#     price
-#     integer($int64)
-#     nullable: true
-#     Целое
-#     число, для
-#     категорий
-#     поле
-#     должно
-#     содержать
-#     null.
+#     @validates('relatives')
+#     def validate_relatives_unique(self, value: list):
+#         if len(value) != len(set(value)):
+#             raise ValidationError('relatives must be unique')
+
+
+class OfferAndCategorySchema(Schema):
+    id = Str(validate=Length(min=1, max=256), strict=True, required=True),
+    name = Str(validate=Length(min=1, max=256), required=True)
+    type = Str(validate=OneOf([types.value for types in ShopUnitType]),
+                 required=True),
+    parentId = Str(validate=Length(min=1, max=256), required=True)
+    price = Int(validate=Range(min=0), strict=True, required=True)
+    child = List(Dict(), required=True)
+
+
+class ImportSchema(Schema):
+    goods = Nested(OfferAndCategorySchema, many=True, required=True,
+                      validate=Length(max=10000))
+    date = Date(format=DATE_FORMAT, required=True)
+
+    @validates_schema
+    def validate_unique_id(self, data, **_):
+        ids = set()
+        for goods in data['offer_and_category']:
+            if goods['id'] in ids:
+                raise ValidationError(
+                    'id %r is not unique' % goods['id']
+                )
+            ids.add(goods['id'])
+
+
+    #проверка, что товар (offer) не является родителем для кого-либо
+    @validates_schema
+    def validate_relatives(self, data, **_):
+        child = {
+            goods['type']: set(goods['child'])
+            for goods in data['offer_and_category']
+        }
+        for types, list_children in child.items():
+            if types == 'OFFER' and len(list_children) != 0:
+                raise ValidationError(
+                    400, 'offer can`t has child'
+                )
+
+!!!!!!!!!!!!!!!
+class ImportIdSchema(Schema):
+    import_id = Int(strict=True, required=True)
+
+
+class ImportResponseSchema(Schema):
+    data = Nested(ImportIdSchema(), required=True)
+
+
+class CitizensResponseSchema(Schema):
+    data = Nested(OfferAndCategorySchema(many=True), required=True)
+
+
+class PatchCitizenResponseSchema(Schema):
+    data = Nested(OfferAndCategorySchema(), required=True)
+
+
+class PresentsSchema(Schema):
+    citizen_id = Int(validate=Range(min=0), strict=True, required=True)
+    presents = Int(validate=Range(min=0), strict=True, required=True)
+
+
+# Схема, содержащая кол-во подарков, которое купят жители по месяцам.
+# Чтобы не указывать вручную 12 полей класс можно сгенерировать.
+CitizenPresentsByMonthSchema = type(
+    'CitizenPresentsByMonthSchema', (Schema,),
+    {
+        str(i): Nested(PresentsSchema(many=True), required=True)
+        for i in range(1, 13)
+    }
+)
+
+
+
+
+
+
+
+class Error(Schema):
+    code = Int(required=True)
+    message = Str(required=True)
+
+
+class ErrorResponseSchema(Schema):
+    error = Nested(Error(), required=True)
